179a2689ec0f50863dabf6ee9c63b2d570525518
yangsben <yangsben@zoeker.cn>
2017-06-08 10:14:24 +0800

    serverTest

---
+++ App/Scripts/serverTest.js
@@ -0,0 +1,13 @@
+/**
+ * Created by yangshibin on 2017/6/5.
+ */
+var http = require('http');
+var server = http.createServer(function (req, res) {
+    req.setEncoding('utf-8');
+    res.writeHead(200, {'Content-type': 'text/html'});
+    res.write('<div style="text-align: center"><h1 style="color: #00b3ee">Node server</h1>' +
+        '<p style="color: #6d6b6a;font-size: 16px">Hello, nice to meet you!</p></div>');
+    res.end();
+});
+server.listen(6090);
+console.log('Server is listening at port 6090!')
